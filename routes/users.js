7a898063dae446fd9d790c02edb2cbb42595f2f0
rexyfahrezi <rexyfahrezi30@gmail.com>
2021-07-17 17:09:45 +0700

    membuat function buatKontak & apiBuatKontak

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,16 @@ router.get('/', function(req, res) {
             auth: oAuth2Client,
         })
     }
+
+    const apiBuatKontak = async function (body) {
+        return await service.people.createContact({
+            personFields: 'names,emailAddresses,phoneNumbers',
+            requestBody : JSON.stringify(body),
+            auth: oAuth2Client,
+        })
+    }
     
-    const tampilKontak = async function () {
+    const apiTampilKontak = async function () {
         return await service.people.connections.list({
             resourceName: 'people/me',
             pageSize: 10,
@@ -28,62 +36,29 @@ router.get('/', function(req, res) {
     
     async function renderData() {
         try {
-
-            const dataKontak = await tampilKontak()
+            
+            //akses google API
+            const dataKontak = await apiTampilKontak();
             const dataUser = await getDataMe();
 
+            //membuat kontak
+            const dataBuat = buatKontak('Budi', '0888999', 'budi@mail');
+            apiBuatKontak(dataBuat);
+            console.log(`[users.js] - Sukses membuat kontak baru`)
+            
+            //ini data user
             const givenName = JSON.stringify(dataUser.data.names[0].givenName);
             const displayName = JSON.stringify(dataUser.data.names[0].displayName);
             const emailAddr = JSON.stringify(dataUser.data.emailAddresses[0].value);
 
+            //menampilkan list kontak
             const arrKontak = dataKontak.data.connections;
-            let listKontak = [];            
+            let listKontak = []
+            renderListcontact(arrKontak, listKontak)
+            console.log('[users.js] - Sukses menampilkan renderListcontact')
 
-            function Kontak(kontaknama, kontakemail, nomorhp) {
-                this.kontaknama = kontaknama;
-                this.kontakemail = kontakemail;
-                this.nomorhp = nomorhp
-            }
-            
-            if(arrKontak) {
-                arrKontak.forEach(function(kontak, i) {
-                    if (!arrKontak[i].emailAddresses){
-                        if(!arrKontak[i].phoneNumbers){
-                            listKontak.push({
-                                "kontaknama": kontak.names[0].displayName,
-                                "kontakemail": "Tidak ada email",
-                                "nomorhp": "Tidak ada nomor handphone"
-                            });
-                        } 
-                        else {
-                            listKontak.push({
-                                "kontaknama": kontak.names[0].displayName,
-                                "kontakemail": "Tidak ada email",
-                                "nomorhp": kontak.phoneNumbers[0].value    
-                        });
-                        }
-                    } 
-                    else if ((!arrKontak[i].phoneNumbers)) {
-                        listKontak.push({
-                            "kontaknama": kontak.names[0].displayName,
-                            "kontakemail": kontak.emailAddresses[0].value,
-                            "nomorhp": "Tidak ada nomor handphone"
-                        });
-                    }
-                    else {
-                        listKontak.push({
-                            "kontaknama": kontak.names[0].displayName,
-                            "kontakemail": kontak.emailAddresses[0].value,
-                            "nomorhp": kontak.phoneNumbers[0].value
-                        });
-                    }
-                });
-            } else {
-                console.log('Tidak ada data kontak')
-            }
-            
-            console.log(arrKontak[1]);
 
+            // render ke page user
             res.render('users', { 
                 title: 'Users Page', 
                 namaLengkapUser: displayName,
@@ -100,36 +75,69 @@ router.get('/', function(req, res) {
 });
 
 
-
-
-
-
-function listConnectionNames(auth) {
-    service.people.connections.list({
-      resourceName: 'people/me',
-      pageSize: 10,
-      personFields: 'names,emailAddresses',
-    }, (err, res) => {
-      if (err) return console.error('The API returned an error: ' + err);
-      const connections = res.data.connections;
-      if (connections) {
-        console.log('Connections:');
-        connections.forEach((person) => {
-          if (person.names && person.names.length > 0) {
-            console.log(person.names[0].displayName);
-          } else {
-            console.log('No display name found for connection.');
-          }
+function renderListcontact(arrKontak, listKontak) {
+          
+    if(arrKontak) {
+        arrKontak.forEach(function(kontak, i) {
+            if (!arrKontak[i].emailAddresses){
+                if(!arrKontak[i].phoneNumbers){
+                    listKontak.push({
+                        "kontaknama": kontak.names[0].displayName,
+                        "kontakemail": "Tidak ada email",
+                        "nomorhp": "Tidak ada nomor handphone"
+                    });
+                } 
+                else {
+                    listKontak.push({
+                        "kontaknama": kontak.names[0].displayName,
+                        "kontakemail": "Tidak ada email",
+                        "nomorhp": kontak.phoneNumbers[0].value    
+                });
+                }
+            } 
+            else if ((!arrKontak[i].phoneNumbers)) {
+                listKontak.push({
+                    "kontaknama": kontak.names[0].displayName,
+                    "kontakemail": kontak.emailAddresses[0].value,
+                    "nomorhp": "Tidak ada nomor handphone"
+                });
+            }
+            else {
+                listKontak.push({
+                    "kontaknama": kontak.names[0].displayName,
+                    "kontakemail": kontak.emailAddresses[0].value,
+                    "nomorhp": kontak.phoneNumbers[0].value
+                });
+            }
         });
-      } else {
-        console.log('No connections found.');
-      }
-    });
-  }
+    } else {
+        console.log('Tidak ada data kontak')
+    }
 
+    return listKontak
+}
 
   
-
+function buatKontak(nama, nohp, email){
+    body={
+        "names": [
+            {
+                "givenName": nama
+            }
+        ],
+        "phoneNumbers": [
+            {
+                'value': nohp
+            }
+        ],
+        "emailAddresses": [
+            {
+                'value': email
+            }
+        ]
+    }
+    return body
+}
 
 
 
